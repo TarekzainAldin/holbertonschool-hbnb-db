a8c99ce4171726ae02768f4325a5e9336407d09a
Francianeny <nirinasoabozy@yahoo.fr>
2024-06-27 00:16:12 +0200

    05_insert_initial_data.sql

--- sql_scripts/05_insert_initial_data.sql
+++ sql_scripts/05_insert_initial_data.sql
@@ -1,3 +1,7 @@
-INSERT INTO State (id, name) VALUES ('1', 'California');
-INSERT INTO City (id, name, state_id) VALUES ('1', 'San Francisco', '1');
-INSERT INTO User (id, email, password, is_admin) VALUES ('1', 'admin@example.com', 'hashed_password', TRUE);
+CREATE TABLE amenities (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
