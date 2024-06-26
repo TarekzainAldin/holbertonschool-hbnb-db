a97ab973f84240f930030560b40fccabef1230a5
Francianeny <nirinasoabozy@yahoo.fr>
2024-06-27 00:14:28 +0200

    04_create_places_table.sql

--- sql_scripts/04_create_places_table.sql
+++ sql_scripts/04_create_places_table.sql
@@ -1,11 +1,16 @@
-CREATE TABLE Place (
+CREATE TABLE places (
     id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
     description TEXT,
-    user_id VARCHAR(36),
-    city_id VARCHAR(36),
+    number_rooms INTEGER DEFAULT 0,
+    number_bathrooms INTEGER DEFAULT 0,
+    max_guest INTEGER DEFAULT 0,
+    price_by_night INTEGER DEFAULT 0,
+    city_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (city_id) REFERENCES City(id)
+    FOREIGN KEY (city_id) REFERENCES cities(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
