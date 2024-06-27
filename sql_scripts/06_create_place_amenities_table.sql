1387ba60aa90abd079ffc3c1c6c06711511c54e0
Francianeny <nirinasoabozy@yahoo.fr>
2024-06-27 13:19:10 +0200

    add to test

--- sql_scripts/06_create_place_amenities_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE place_amenities (
-    place_id VARCHAR(36) NOT NULL,
-    amenity_id VARCHAR(36) NOT NULL,
-    PRIMARY KEY (place_id, amenity_id),
-    FOREIGN KEY (place_id) REFERENCES places(id),
-    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
-);
